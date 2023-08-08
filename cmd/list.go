627a70b7f732a45866bb3c9e8ea58d9c227cbf72
Mangaal <angommeeteimangaal@gmail.com>
2023-08-08 22:54:44 +0530

    cli options update

--- cmd/list.go
+++ cmd/list.go
@@ -16,13 +16,8 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "ls",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "store ls will list all the files",
+	Long:  `.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("list called")
 
--- cmd/wc.go
+++ cmd/wc.go
@@ -17,12 +17,7 @@ import (
 var wcCmd = &cobra.Command{
 	Use:   "wc",
 	Short: "store wc",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("wc called")
 
