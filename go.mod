cc6d05f3fbe3c9d2ba554d711bbfaf173ffb903f
Mangaal <angommeeteimangaal@gmail.com>
2023-08-09 19:15:38 +0530

    go binary for diff platform

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module store-cli
 
 go 1.12
 
-require github.com/spf13/cobra v1.7.0
+require (
+	github.com/mitchellh/gox v1.0.1 // indirect
+	github.com/spf13/cobra v1.7.0
+)
--- install.sh
+++ install.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <platform>"
+    exit 1
+fi
+
 
 # Set the necessary environment variables
 CLI_NAME="store"
 INSTALL_PATH="/usr/local/bin"
-CLI_URL="https://github.com/Mangaal/store-cli/raw/main/downloads/store"
+
+case "$1" in
+    windows)
+        CLI_URL="https://github.com/Mangaal/store-cli/raw/main/downloads/store-windows-amd64.exe"
+        ;;
+    linux)
+        CLI_URL="https://github.com/Mangaal/store-cli/raw/main/downloads/store-linux-amd64"
+        ;;
+    darwin)
+        CLI_URL="https://github.com/Mangaal/store-cli/raw/main/downloads/store-darwin-amd64"
+        ;;
+    *)
+        echo "Invalid argument. Supported options: latest, windows, linux, darwin."
+        exit 1
+        ;;
+esac
+
 
 # Download the latest release of your CLI
 curl -Lo "$CLI_NAME" "$CLI_URL"
